ca168e2849390865aa79d53bf9c7c8b13882c131
Troy DeSpain <despaintroy@gmail.com>
2023-02-03 19:38:38 -0700

    week total time

--- src/pages/TimeEntryIndex/TimeEntryIndex.tsx
+++ src/pages/TimeEntryIndex/TimeEntryIndex.tsx
@@ -8,8 +8,9 @@ import {Alert, AlertTitle, Container} from "@mui/material"
 import {Project, Task, TimeEntry} from "api/sdk"
 import PageHeader from "components/PageHeader"
 import dayjs from "dayjs"
-import React, {FC, useContext, useState} from "react"
+import React, {FC, useContext, useEffect, useState} from "react"
 import {AuthContext} from "utils/context"
+import {dateToISO} from "utils/helpers"
 import {AddTimeEntryButton} from "./AddTimeEntry"
 
 import duration from "dayjs/plugin/duration"
@@ -19,6 +20,33 @@ export const TimeEntryIndex: FC = () => {
   const {session, currentUser} = useContext(AuthContext)
 
   const [refreshTrigger, setRefreshTrigger] = useState(0)
+  const [weeklyTime, setWeeklyTime] = useState(dayjs.duration(0, "minutes"))
+
+  useEffect(() => {
+    session.timeEntry
+      .query({
+        condition: {
+          And: [
+            {user: {Equal: currentUser._id}},
+            {
+              date: {
+                GreaterThanOrEqual: dateToISO(dayjs().startOf("week").toDate())
+              }
+            }
+          ]
+        }
+      })
+      .then((timeEntries) => {
+        const durations = timeEntries.map((timeEntry) =>
+          dayjs.duration(timeEntry.duration)
+        )
+        const totalTime = durations.reduce(
+          (prev, curr) => prev.add(curr),
+          dayjs.duration(0, "minutes")
+        )
+        setWeeklyTime(totalTime)
+      })
+  }, [refreshTrigger])
 
   const annotatedTimeEntryEndpoint: SessionRestEndpoint<
     WithAnnotations<TimeEntry, {task?: Task; project?: Project}>
@@ -52,6 +80,11 @@ export const TimeEntryIndex: FC = () => {
           afterSubmit={() => setRefreshTrigger((prev) => prev + 1)}
         />
       </PageHeader>
+
+      <Alert severity="info" sx={{mb: 2}} variant="outlined">
+        <AlertTitle>Weekly Time</AlertTitle>
+        You have worked {weeklyTime.format("HH:mm")} this week.
+      </Alert>
 
       <RestDataTable
         restEndpoint={annotatedTimeEntryEndpoint}

9aaa07ac79296b4e7f706d1b803478568a0d9fff
Brady Svedin <brady@lightningkite.com>
2023-02-28 15:19:52 -0700

    Again simplified the filter process. Removed the two calls for tasks with one call, and set conditions above.

--- src/pages/Dashboard/ProjectsTasks.tsx
+++ src/pages/Dashboard/ProjectsTasks.tsx
@@ -1,6 +1,14 @@
 import {Condition} from "@lightningkite/lightning-server-simplified"
 import {ExpandMore} from "@mui/icons-material"
-import {Accordion, AccordionDetails, AccordionSummary, Box, Chip, List, Typography} from "@mui/material"
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Chip,
+  List,
+  Typography
+} from "@mui/material"
 import {Project, TaskState} from "api/sdk"
 import {AddTaskButton} from "components/AddTaskButton"
 import ErrorAlert from "components/ErrorAlert"
@@ -11,185 +19,185 @@ import {booleanCompare, compareTasksByState} from "utils/helpers"
 import {AnnotatedTask, useAnnotatedEndpoints} from "utils/useAnnotatedEndpoints"
 import {TaskListItem} from "./TaskListItem"
 
-export const ProjectsTasks: FC<{ onlyMine: boolean }> = ({onlyMine}) => {
-    const {session, currentOrganization, currentUser} = useContext(AuthContext)
-    const {timers} = useContext(TimerContext)
-    const {annotatedTaskEndpoint} = useAnnotatedEndpoints()
+export const ProjectsTasks: FC<{onlyMine: boolean}> = ({onlyMine}) => {
+  const {session, currentOrganization, currentUser} = useContext(AuthContext)
+  const {timers} = useContext(TimerContext)
+  const {annotatedTaskEndpoint} = useAnnotatedEndpoints()
 
-    const [projects, setProjects] = useState<Project[] | null>()
-    const [annotatedTasks, setAnnotatedTasks] = useState<AnnotatedTask[] | null>()
-    const [initialSorting, setInitialSorting] = useState<string[]>()
+  const [projects, setProjects] = useState<Project[] | null>()
+  const [annotatedTasks, setAnnotatedTasks] = useState<AnnotatedTask[] | null>()
+  const [initialSorting, setInitialSorting] = useState<string[]>()
 
-    const refreshDashboardData = async () => {
-        session.project
-            .query({condition: {organization: {Equal: currentOrganization._id}}})
-            .then(setProjects)
-            .catch(() => setProjects(null))
+  const refreshDashboardData = async () => {
+    session.project
+      .query({condition: {organization: {Equal: currentOrganization._id}}})
+      .then(setProjects)
+      .catch(() => setProjects(null))
 
-        let stateCondition: Condition<AnnotatedTask>
-        if (currentUser.defaultFilters.states.length === 0) {
-            stateCondition = {
-                state: {Equal: TaskState.Active}
-            }
-        } else {
-            stateCondition = {
-                state: {Inside: currentUser.defaultFilters.states}
-            }
-        }
+    let stateCondition: Condition<AnnotatedTask>
+    if (currentUser.defaultFilters.states.length === 0) {
+      stateCondition = {
+        state: {Equal: TaskState.Active}
+      }
+    } else {
+      stateCondition = {
+        And: [
+          {state: {Inside: currentUser.defaultFilters.states}},
+          {state: {NotInside: [TaskState.Cancelled, TaskState.Delivered]}}
+        ]
+      }
+    }
 
-        if (currentUser.defaultFilters.projects.length === 0) {
+    let projectCondition: Condition<AnnotatedTask>
+    let userCondition: Condition<AnnotatedTask>
+    if (currentUser.defaultFilters.projects.length === 0) {
+      projectCondition = {Always: true}
+      userCondition = {user: {Equal: currentUser._id}}
+    } else {
+      userCondition = {Always: true}
+      projectCondition = {
+        project: {Inside: currentUser.defaultFilters.projects}
+      }
+    }
 
-            await annotatedTaskEndpoint
-                .query({
-                    condition: {
-                        And: [
-                            {organization: {Equal: currentOrganization._id}},
-                            {user: {Equal: currentUser._id}},
-                            stateCondition,
-                        ]
-                    }
-                })
-                .then(setAnnotatedTasks)
-                .catch(() => setAnnotatedTasks(null))
-
-        } else {
-
-            const conditions: Condition<AnnotatedTask>[] = [
+    await annotatedTaskEndpoint
+      .query({
+        condition: {
+          Or: [
+            {
+              And: [
                 {organization: {Equal: currentOrganization._id}},
                 stateCondition,
-            ]
-
-            await annotatedTaskEndpoint
-                .query({
-                    condition: {
-                        Or: [
-                            {
-                                And: [
-                                    ...conditions,
-                                    {project: {Inside: currentUser.defaultFilters.projects}}
-                                ]
-                            },
-                            {
-                                And: [
-                                    {user: {Equal: currentUser._id}},
-                                    stateCondition
-                                ]
-                            }
-                        ]
-                    }
-                })
-                .then(setAnnotatedTasks)
-                .catch(() => setAnnotatedTasks(null))
-
+                projectCondition,
+                userCondition
+              ]
+            },
+            {
+              And: [
+                {user: {Equal: currentUser._id}},
+                {
+                  state: {
+                    Inside: [
+                      TaskState.Active,
+                      ...currentUser.defaultFilters.states
+                    ]
+                  }
+                },
+                {state: {NotInside: [TaskState.Cancelled, TaskState.Delivered]}}
+              ]
+            }
+          ]
         }
+      })
+      .then(setAnnotatedTasks)
+      .catch(() => setAnnotatedTasks(null))
+  }
 
-    }
+  useEffect(() => {
+    refreshDashboardData()
+  }, [Object.values(timers).length])
 
-    useEffect(() => {
-        refreshDashboardData()
-    }, [Object.values(timers).length])
+  const tasksByProject = useMemo(() => {
+    if (!annotatedTasks || !projects) return {}
 
-    const tasksByProject = useMemo(() => {
-        if (!annotatedTasks || !projects) return {}
+    const tasksByProject: Record<
+      string,
+      {projectTasks: AnnotatedTask[]; myTasksCount: number}
+    > = {}
 
-        const tasksByProject: Record<
-            string,
-            { projectTasks: AnnotatedTask[]; myTasksCount: number }
-        > = {}
+    projects.forEach((project) => {
+      const projectTasks = annotatedTasks.filter(
+        (task) =>
+          task.project === project._id &&
+          (!onlyMine || task.user === currentUser._id)
+      )
 
-        projects.forEach((project) => {
-            const projectTasks = annotatedTasks.filter(
-                (task) =>
-                    task.project === project._id &&
-                    (!onlyMine || task.user === currentUser._id)
-            )
+      const myTasksCount = projectTasks.filter(
+        (task) => task.user === currentUser._id
+      ).length
 
-            const myTasksCount = projectTasks.filter(
-                (task) => task.user === currentUser._id
-            ).length
+      tasksByProject[project._id] = {projectTasks, myTasksCount}
+    })
 
-            tasksByProject[project._id] = {projectTasks, myTasksCount}
-        })
+    return tasksByProject
+  }, [annotatedTasks, projects, onlyMine])
 
-        return tasksByProject
-    }, [annotatedTasks, projects, onlyMine])
+  useEffect(() => {
+    if (!annotatedTasks || !projects || initialSorting) return
 
-    useEffect(() => {
-        if (!annotatedTasks || !projects || initialSorting) return
-
-        setInitialSorting(
-            Object.entries(tasksByProject)
-                .sort((a, b) => a[1].myTasksCount - b[1].myTasksCount)
-                .map(([projectName]) => projectName)
-        )
-    }, [tasksByProject])
-
-    if (
-        projects === undefined ||
-        annotatedTasks === undefined ||
-        !initialSorting
-    ) {
-        return <Loading/>
-    }
-
-    if (projects === null || annotatedTasks === null) {
-        return <ErrorAlert>Error loading tasks</ErrorAlert>
-    }
-
-    return (
-        <Box>
-            {Object.entries(tasksByProject)
-                .filter(([_, {projectTasks}]) => projectTasks.length > 0)
-                .sort(
-                    (a, b) => initialSorting.indexOf(b[0]) - initialSorting.indexOf(a[0])
-                )
-                .map(([projectId, {projectTasks, myTasksCount}]) => {
-                    const project = projects.find((p) => p._id === projectId)
-
-                    return (
-                        <Accordion key={projectId} defaultExpanded={myTasksCount > 0}>
-                            <AccordionSummary expandIcon={<ExpandMore/>}>
-                                <Chip
-                                    label={myTasksCount}
-                                    size="small"
-                                    color="primary"
-                                    sx={{mr: 2, visibility: myTasksCount ? "visible" : "hidden"}}
-                                />
-                                <Typography variant="h2">{project?.name}</Typography>
-                                <AddTaskButton
-                                    project={project}
-                                    afterSubmit={(newAnnotatedTask) =>
-                                        setAnnotatedTasks([
-                                            ...(annotatedTasks ?? []),
-                                            newAnnotatedTask
-                                        ])
-                                    }
-                                    sx={{ml: "auto", mr: 2}}
-                                />
-                            </AccordionSummary>
-                            <AccordionDetails sx={{p: 0}}>
-                                <List>
-                                    {projectTasks
-                                        // Sort by emergency, state, then by is current user
-                                        .sort((a, b) => {
-                                            return (
-                                                booleanCompare(a, b, (t) => t.emergency) ||
-                                                compareTasksByState(a, b) ||
-                                                booleanCompare(a, b, (t) => t.user === currentUser._id)
-                                            )
-                                        })
-                                        .map((task) => (
-                                            <TaskListItem
-                                                annotatedTask={task}
-                                                refreshDashboard={refreshDashboardData}
-                                                key={task._id}
-                                            />
-                                        ))}
-                                </List>
-                            </AccordionDetails>
-                        </Accordion>
-                    )
-                })}
-        </Box>
+    setInitialSorting(
+      Object.entries(tasksByProject)
+        .sort((a, b) => a[1].myTasksCount - b[1].myTasksCount)
+        .map(([projectName]) => projectName)
     )
+  }, [tasksByProject])
+
+  if (
+    projects === undefined ||
+    annotatedTasks === undefined ||
+    !initialSorting
+  ) {
+    return <Loading />
+  }
+
+  if (projects === null || annotatedTasks === null) {
+    return <ErrorAlert>Error loading tasks</ErrorAlert>
+  }
+
+  return (
+    <Box>
+      {Object.entries(tasksByProject)
+        .filter(([_, {projectTasks}]) => projectTasks.length > 0)
+        .sort(
+          (a, b) => initialSorting.indexOf(b[0]) - initialSorting.indexOf(a[0])
+        )
+        .map(([projectId, {projectTasks, myTasksCount}]) => {
+          const project = projects.find((p) => p._id === projectId)
+
+          return (
+            <Accordion key={projectId} defaultExpanded={myTasksCount > 0}>
+              <AccordionSummary expandIcon={<ExpandMore />}>
+                <Chip
+                  label={myTasksCount}
+                  size="small"
+                  color="primary"
+                  sx={{mr: 2, visibility: myTasksCount ? "visible" : "hidden"}}
+                />
+                <Typography variant="h2">{project?.name}</Typography>
+                <AddTaskButton
+                  project={project}
+                  afterSubmit={(newAnnotatedTask) =>
+                    setAnnotatedTasks([
+                      ...(annotatedTasks ?? []),
+                      newAnnotatedTask
+                    ])
+                  }
+                  sx={{ml: "auto", mr: 2}}
+                />
+              </AccordionSummary>
+              <AccordionDetails sx={{p: 0}}>
+                <List>
+                  {projectTasks
+                    // Sort by emergency, state, then by is current user
+                    .sort((a, b) => {
+                      return (
+                        booleanCompare(a, b, (t) => t.emergency) ||
+                        compareTasksByState(a, b) ||
+                        booleanCompare(a, b, (t) => t.user === currentUser._id)
+                      )
+                    })
+                    .map((task) => (
+                      <TaskListItem
+                        annotatedTask={task}
+                        refreshDashboard={refreshDashboardData}
+                        key={task._id}
+                      />
+                    ))}
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })}
+    </Box>
+  )
 }

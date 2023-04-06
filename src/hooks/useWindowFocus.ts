6c47180a0abd3e822f2cc0454a7187f2ebb65877
Troy DeSpain <despaintroy@gmail.com>
2023-04-06 15:43:32 -0600

    Create useWindowFocus hook

---
+++ src/hooks/useWindowFocus.ts
@@ -0,0 +1,24 @@
+import React, {useState} from "react"
+
+export default function useWindowFocus(): boolean {
+  const [windowFocus, setWindowFocus] = useState(
+    document.hasFocus() && !document.hidden
+  )
+
+  console.log("windowFocus", windowFocus)
+
+  React.useEffect(() => {
+    const onFocus = () => setWindowFocus(true)
+    const onBlur = () => setWindowFocus(false)
+
+    window.addEventListener("focus", onFocus)
+    window.addEventListener("blur", onBlur)
+
+    return () => {
+      window.removeEventListener("focus", onFocus)
+      window.removeEventListener("blur", onBlur)
+    }
+  }, [])
+
+  return windowFocus
+}

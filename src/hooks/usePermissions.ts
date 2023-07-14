2488567499550e8e0686a15dc914363341f77289
Troy DeSpain <despaintroy@gmail.com>
2023-07-14 10:38:23 -0600

    Organize user permissions

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -11,49 +11,49 @@ export function usePermissions() {
     return !!role && roles.includes(role)
   }
 
+  // Assign `false` to a permission if only super users and owners can do it
   const usePermissionsReturn = {
-    canBeAssignedTasks: isA(
-      UserRole.InternalTeamMember,
-      UserRole.ExternalTeamMember,
-      UserRole.Contractor
-    ),
-
-    canSubmitTime: isA(UserRole.InternalTeamMember, UserRole.Contractor),
-
-    canManageAllTime: false as boolean,
-
-    canManageAllTasks: isA(UserRole.InternalTeamMember),
-
-    canReportNewTasks: true,
-
-    canViewInternalReports: isA(UserRole.InternalTeamMember),
-
-    canViewClientReports: isA(UserRole.Client),
-
+    // USERS
     canManageAllUsers: false as boolean,
-
-    canManageAllProjects: isA(UserRole.InternalTeamMember),
-
-    canViewProjectsTab:
-      isA(UserRole.InternalTeamMember) || !!limitToProjects?.length,
-
-    canViewIndividualTimeEntries: isA(
-      UserRole.InternalTeamMember,
-      UserRole.Contractor
-    ),
-
     canViewIndividualUsers: isA(
       UserRole.InternalTeamMember,
       UserRole.Contractor
     ),
 
+    // TIME
+    canSubmitTime: isA(UserRole.InternalTeamMember, UserRole.Contractor),
+    canManageAllTime: false as boolean,
+    canViewIndividualTimeEntries: isA(
+      UserRole.InternalTeamMember,
+      UserRole.Contractor
+    ),
+
+    // PROJECTS
+    canManageAllProjects: isA(UserRole.InternalTeamMember),
+    canViewProjectsTab:
+      isA(UserRole.InternalTeamMember) || !!limitToProjects?.length,
+
+    // TASKS
+    canDeliverTasks: isA(UserRole.Client),
+    canReportNewTasks: true as boolean,
+    canManageAllTasks: isA(UserRole.InternalTeamMember),
+    canBeAssignedTasks: isA(
+      UserRole.InternalTeamMember,
+      UserRole.ExternalTeamMember,
+      UserRole.Contractor
+    ),
     doesCareAboutPRs: isA(
       UserRole.InternalTeamMember,
       UserRole.Contractor,
       UserRole.ExternalTeamMember
-    )
+    ),
+
+    // REPORTS
+    canViewInternalReports: isA(UserRole.InternalTeamMember),
+    canViewClientReports: isA(UserRole.Client)
   } satisfies Record<string, boolean>
 
+  // Super users and owners can do everything
   if (isSuperUser || isA(UserRole.Owner)) {
     Object.keys(usePermissionsReturn).forEach((key) => {
       usePermissionsReturn[key as keyof typeof usePermissionsReturn] = true
